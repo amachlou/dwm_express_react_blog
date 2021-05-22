427b3179ced8ead95fef02edcbb4ba68dfb8b485
Jijinet <jihanrabat345@gmail.com>
2021-05-22 18:41:50 +0100

    modify users route commit

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
-const router = require('express').Router();
-const usersRepo = require('../repositories/users')
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
+ 
+ const router = require('express').Router();
+ const usersRepo = require('../repositories/users')
+ /* GET users listing. */
+ router.get('/', async function(req, res, next) {
+   res.send(await usersRepo.getAllUsers())
+ });
+ module.exports = router;
