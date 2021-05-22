96d47e1c0990ca1fb70fdd978c8ee5f68f44b1e2
Jijinet <jihanrabat345@gmail.com>
2021-05-22 18:37:08 +0100

    third commit

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router();
+const usersRepo = require('../repositories/users')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
